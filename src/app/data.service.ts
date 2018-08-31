40de7b8f6043258c5ccd737575dffe20449649aa
karan <karan@darwinlabs.io>
2018-08-31 22:56:37 +0530

    err

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getUsers() {
-    return this.http.get('http://temp.griffex.co/v1/verify/user/exist',
+    return this.http.get('https://temp.griffex.co/v1/verify/user/exist',
     {
       headers: {'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiY2Q5M2Q5YTEtMmY3Zi00NzU3LTg3NzMtZDAwNzhhNjAxNTM3IiwiaWF0IjoxNTM1NzMzMjQ1LCJleHAiOjE1NjcyNjkyNDV9.KJod0M2QXHgHRYfvuuSganZXhT158KbVCkYP_T0aL3A'}
    }   )
